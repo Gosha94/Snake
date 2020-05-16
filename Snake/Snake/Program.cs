06b21a6195ad4cb427d1548b53d55b4848ab8b7d
Gosha94 <gerizch@rambler.ru>
2020-05-16 15:16:34 +0300

    16_05 Процедурный подход

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -6,8 +6,28 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.ReadKey();
+            int x1 = 0;
+            int y1 = 0;
+            string sym1 = "&";
+            DrawPoint(x1, y1, sym1);
+
+            int x2 = 1;
+            int y2 = 1;
+            string sym2 = "&";
+            DrawPoint(x2, y2, sym2);
+
+            int x3 = 2;
+            int y3 = 2;
+            string sym3 = "#";
+            DrawPoint(x3, y3, sym3);
+
+            Console.ReadLine();
+        }
+
+        static void DrawPoint(int x, int y, string sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
